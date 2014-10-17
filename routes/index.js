3e4096b17a8677429e646733c0b6da0baabba429
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2014-10-17 14:05:22 +0100

    Request forwarding

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -22,6 +23,12 @@ router.get('/foo', function(req, res) {
 	
 	//res.render('index', { title: 'Hot Reloaded grunt' });
 	res.render('foo', {});
+});
+
+router.get('/forward', function(req, res) {
+	
+	var newurl = 'https://gist.githubusercontent.com/Keeguon/2310008/raw/865a58f59b9db2157413e7d3d949914dbf5a237d/countries.json';
+	request(newurl).pipe(res);
 });
 
 
