399abbccadb87ac17d14644c721131b7f926128e
Kevin Sheedy <kevin.x.sheedy@aib.ie>
2015-01-19 11:22:40 +0000

    Allow for plugins

--- app/plugins.js
+++ app/plugins.js
@@ -1,13 +1,13 @@
 module.exports = function(app){
-	console.log('user app:');
+	console.log('Load plugins...:');
 
 	var express = require('express');
 	var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-	res.send('respond with a userapp');
-});
+	/* GET users listing. */
+	router.get('/', function(req, res) {
+		res.send('respond with a userapp');
+	});
 
 	app.use('/userapp', router);
 }
